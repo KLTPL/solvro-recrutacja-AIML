9c5ed9e9708838cbf8e88a2ab8c0af77f7ef3c78
KLTPL <kltpl@duck.com>
2026-03-19 00:04:18 +0100

    feat: add argparse CLI library and split code info single functions

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
 client = OpenAI()
 
-with open("samples/input_file.txt", "r", encoding="utf-8") as file:
-    file_content = file.read()
 
-res = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": f"Podsumuj to: {file_content}"}],
-)
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Streszczanie artykułów naukowych z AI"
+    )
 
-print(res.choices[0].message.content)
+    parser.add_argument(
+        "filepath", type=str, help="Ścieżka do pliku .txt który chcesz streścić"
+    )
+
+    return parser.parse_args()
+
+
+def read_file(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"Plik nie znaleziony {file_path}")
+        return None
+
+
+def summarize_text(file_content):
+    print("Czytanie i analizowanie pliku")
+    res = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": f"Podsumuj to: {file_content}"}],
+    )
+    return res.choices[0].message.content
+
+
+def main():
+    args = parse_args()
+
+    file_content = read_file(args.filepath)
+
+    if file_content:
+        print(file_content)
+
+
+if __name__ == "__main__":
+    main()
